3e3520bb498e23081914f26a248c653673707a2a
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-12-25 15:54:08 +0100

    working on webpack config

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import * as data from './data.js';
 import displayUserCard from './displayUserCard.js';
+import _ from 'lodash';
+import '../css/main.scss';
 
 const userCardElem = document.querySelector('.userCard');
 const titleElem = document.querySelector('title');
 const h1Elem = document.querySelector('h1');
+const lodashExampleElem = document.querySelector('.lodashExample');
 
 userCardElem.innerHTML = displayUserCard(data.user); 
 titleElem.innerHTML = data.pageTitle; 
 h1Elem.innerHTML = data.pageTitle; 
+lodashExampleElem.innerHTML = `This is version ${_.VERSION} and this is ${_.camelCase('camel case')}.`;
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+import path from 'path';
+const __dirname = path.resolve(path.dirname(''));
+
+const config = {
+	entry: './src/index.js',
+	output: {
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: [
+					'style-loader',
+					'css-loader',
+					'sass-loader'
+				]
+			}
+		]
+	}
+}
+
+export default config;
